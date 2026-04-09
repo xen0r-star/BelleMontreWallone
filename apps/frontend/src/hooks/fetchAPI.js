e7698fdbd9c0f075cefcc1a87e921624ff05d092
Tomus <205559180+TomusLeVrai@users.noreply.github.com>
2026-04-09 15:05:36 +0200

    🖥️ Modification of frontend | Coed by Tomus 🕶️

---
+++ apps/frontend/src/hooks/fetchAPI.js
@@ -0,0 +1,33 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+import { useEffect } from "react";
+
+export function fetchWatches(setWatches, setIsLoading) {
+   useEffect(() => {
+        setIsLoading(true);
+        
+        const fetchWatches = async () => {
+            try {
+                const response = await fetch(`${API_URL}/watches`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                });
+                if (!response.ok) throw new Error(`Erreur requête HTTP : ${response.status}`);
+                const { data } = await response.json();
+                if (data) {
+                    setWatches(data);
+                } else {
+                    throw new Error(`Erreur requête HTTP : aucune data`);
+                }
+                
+            } catch (e) {
+                console.error("Erreur lors du chargement des données:", e);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchWatches();
+    }, [])
+}
